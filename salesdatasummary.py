0327f44152ac0509e3d3ee1016b76184747ed824
arulrajj <arulraj.infotech@gmail.com>
2025-09-11 11:14:33 +0530

    Delete salesdatasummary.py

--- salesdatasummary.py
+++
@@ -1,6 +0,0 @@
-
-sales = [1200, 3400, 560, 4500, 2100]
-print(f"Total Sales:{sum(sales)}")
-print(f"Average Sales:{sum(sales) / len(sales)}")
-print(f"Highest Sale:{max(sales)}")
-print(f"Lowest Sale:{min(sales)}")
